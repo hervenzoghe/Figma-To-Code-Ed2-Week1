b1dd3b7c4be6374a5565ac53ea50793208459e1c
Hervé Zossou <hervezossou8@gmail.com>
2024-08-02 10:24:29 +0100

    Reviewing project structure

--- source/data.js
+++ source/data.js
@@ -4,32 +4,32 @@ let data = {
         collectionName: "CyberPunk",
         category: "Gaming",
         price: "68",
-        image: "./public/images/cyberpunk.png",
-        icon: "./public/icons/ethereum-ellipse.png",
+        image: "/public/images/cyberpunk.png",
+        icon: "/public/icons/ethereum-ellipse.png",
         buttonText: "Place a Bid",
       },
       {
         collectionName: "Durolost  Boll - Upper",
         category: "Art",
         price: "68",
-        image: "./public/images/durolost.png",
-        icon: "./public/icons/ethereum-ellipse.png",
+        image: "/public/images/durolost.png",
+        icon: "/public/icons/ethereum-ellipse.png",
         buttonText: "Place a Bid",
       },
       {
         collectionName: "Space X Wiper",
         category: "Art",
         price: "68",
-        image: "./public/images/spaceXwiper.png",
-        icon: "./public/icons/ethereum-ellipse.png",
+        image: "/public/images/spaceXwiper.png",
+        icon: "/public/icons/ethereum-ellipse.png",
         buttonText: "Place a Bid",
       },
       {
         collectionName: "Snoop Dogg",
         category: "Celebrities",
         price: "68",
-        image: "./public/images/snoopdog.png",
-        icon: "./public/icons/ethereum-ellipse.png",
+        image: "/public/images/snoopdog.png",
+        icon: "/public/icons/ethereum-ellipse.png",
         buttonText: "Place a Bid",
       },
     ],
@@ -37,22 +37,22 @@ let data = {
     cardInfo: [
         {
             title: "Set up your wallet",
-            icon: "./public/icons/wallet.png",
+            icon: "/public/icons/wallet.png",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
         },
         {
             title: "Add your NFT'\s",
-            icon: "./public/icons/cloud-download.png",
+            icon: "/public/icons/cloud-download.png",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
         },
         {
             title: "Promote your NFT'\s",
-            icon: "./public/icons/sale-tag.png",
+            icon: "/public/icons/sale-tag.png",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
         },
         {
             title: "Sell your NFT'\s",
-            icon: "./public/icons/bitcoin-ellipse.png",
+            icon: "/public/icons/bitcoin-ellipse.png",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
         },
     ]
